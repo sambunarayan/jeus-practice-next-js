311602279fbd3e92c455752f0f353a347598b911
sambunarayan <soyou30257@gmail.com>
2023-04-10 22:42:50 +0900

    Learned the basics of React

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import {Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Counter from './pages/Counter';
+import Input from './pages/input';
+import Input2 from './pages/input2';
+import UserList from './pages/UserList';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> | 
+        <Link to="/input">Input</Link> | <Link to="/input2">Input2</Link>| <Link to="/UserList">UserList</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/input2" element={<Input2 />} />
+        <Route path="/UserList" element={<UserList />} />
       </Routes>
     </div>
   );
