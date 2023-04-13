f1d14cd58d3ff9582f8fa0b99e5a290755521481
Jaewoo Kim <soyou30257@gmail.com>
2023-04-13 20:54:32 +0900

    clean up the code

---
+++ jeus-react-practical-app/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return <h1>This is home page.</h1>
+}
+
+export default Home;
+
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>This is home page.</h1>
+//     }
+// }
+
+// export default Home;
